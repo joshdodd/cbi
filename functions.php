f9e82e99503f6b5dbe751419acf61c17250d6e21
meshdesign <joshdodd@meshfresh.com>
2015-02-06 09:02:49 -0500

    Dashboard cleanup

--- functions.php
+++ functions.php
@@ -65,13 +65,13 @@ add_action('login_head', 'my_custom_login_logo'); //Add custom logo to admin
 function remove_dashboard_widgets(){
 
     //remove_meta_box('dashboard_right_now','dashboard','core'); // right now overview box
-    //remove_meta_box('dashboard_incoming_links','dashboard','core'); // incoming links box
-    //remove_meta_box('dashboard_quick_press','dashboard','core'); // quick press box
+    remove_meta_box('dashboard_incoming_links','dashboard','core'); // incoming links box
+    remove_meta_box('dashboard_quick_press','dashboard','core'); // quick press box
     //remove_meta_box('dashboard_plugins','dashboard','core'); // new plugins box
     //remove_meta_box('dashboard_recent_drafts','dashboard','core'); // recent drafts box
-    //remove_meta_box('dashboard_recent_comments','dashboard','core'); // recent comments box
-    //remove_meta_box('dashboard_primary','dashboard','core'); // wordpress development blog box
-    //remove_meta_box('dashboard_secondary','dashboard','core'); // other wordpress news box
+    remove_meta_box('dashboard_recent_comments','dashboard','core'); // recent comments box
+    remove_meta_box('dashboard_primary','dashboard','core'); // wordpress development blog box
+    remove_meta_box('dashboard_secondary','dashboard','core'); // other wordpress news box
 } 
 
 // Remove menus froms the admin area 
@@ -80,11 +80,11 @@ function delete_menu_items() {
     /*** Remove menu http://codex.wordpress.org/Function_Reference/remove_menu_page 
     syntaxe : remove_menu_page( $menu_slug )  **/
     //remove_menu_page('index.php'); // Dashboard
-    //remove_menu_page('edit.php'); // Posts
+    remove_menu_page('edit.php'); // Posts
     //remove_menu_page('upload.php'); // Media
     //remove_menu_page('link-manager.php'); // Links
     //remove_menu_page('edit.php?post_type=page'); // Pages
-    //remove_menu_page('edit-comments.php'); // Comments
+    remove_menu_page('edit-comments.php'); // Comments
     //remove_menu_page('themes.php'); // Appearance
     //remove_menu_page('plugins.php'); // Plugins
     //remove_menu_page('users.php'); // Users
